e97797b48ec3f8e0fef29dd9acce43d04dc63dc2
jiajingbin <jiajaybin@126.com>
2025-03-31 17:45:13 +0800

    feat: add deploy-taostest action

---
+++ .github/scripts/install_via_apt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+install_packages() {
+    local packages=("$@")
+
+    for package in "${packages[@]}"; do
+        if ! dpkg -l | grep -q "$package"; then
+            echo "Installing $package..."
+            if ! apt-get install -y "$package"; then
+                if ! apt-get install -y --fix-missing "$package"; then
+                    echo "Attempting to update and install $package..."
+                    apt-get update -qq && apt-get install -y "$package"
+                fi
+            fi
+        else
+            echo "$package is already installed."
+        fi
+    done
+}
+
+install_packages "$@"
