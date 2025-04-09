e54e3582d9d0ff05e0b395af6e5e94c218399845
jiajingbin <jiajaybin@126.com>
2025-04-09 20:33:32 +0800

    enh: Encapsulation function

--- .github/scripts/prepare_offline_env.sh
+++ .github/scripts/prepare_offline_env.sh
@@ -34,55 +34,85 @@ if [ -z "$system_packages" ] && [ -z "$python_packages" ]; then
     exit 1
 fi
 
-if [ -n "$system_packages" ]; then
-    echo "Downloading system packages: $system_packages"
-    if [ -f /etc/redhat-release ]; then
-        # TODO
-        yum install -y yum-utils
-        # yum install -y $formated_system_packages --downloadonly --downloaddir="$system_packages_dir" --setopt=installonly=False
-        repotrack $formated_system_packages --download-path="$system_packages_dir"
-    elif [ -f /etc/debian_version ]; then
-        # TODO
-        apt-get install --download-only -y $formated_system_packages -o Dir::Cache::archives="$system_packages_dir"
+function config_yum() {
+    # Define the line to be added
+    EXCLUDE_LINE="exclude=*.i?86"
+
+    # Check if the /etc/yum.conf file exists
+    if [ -f /etc/yum.conf ]; then
+        # Check if the line already exists
+        if grep -q "^exclude=*.i?86" /etc/yum.conf; then
+            echo "The line already exists in /etc/yum.conf."
+        else
+            # Append the line after the [main] section
+            sed -i "/^\[main\]/a $EXCLUDE_LINE" /etc/yum.conf
+            echo "Successfully added '$EXCLUDE_LINE' to /etc/yum.conf."
+        fi
     else
-        echo "Unsupported Linux distribution.. Please install the packages manually."
+        echo "/etc/yum.conf file does not exist."
     fi
-else
-    echo "No system packages to install."
-fi
+}
 
-if [ -n "$python_packages" ]; then
-    echo "Installing uv and Python $python_version and packages: $python_packages"
-
-    # Install uv with setup_env.sh
-    if ! command -v uv &> /dev/null; then
-        wget -O "$script_dir"/setup_env.sh https://raw.githubusercontent.com/taosdata/TDengine/main/packaging/setup_env.sh
-        chmod +x "$script_dir"/setup_env.sh
-        "$script_dir"/setup_env.sh install_uv
-    fi
-
-    if [ -f "$HOME/.local/bin/env" ]; then
-        source "$HOME/.local/bin/env"
+function install_system_packages() {
+    if [ -n "$system_packages" ]; then
+        echo "Downloading system packages: $system_packages"
+        if [ -f /etc/redhat-release ]; then
+            # TODO Confirm
+            config_yum
+            yum install -y yum-utils
+            # yum install -y $formated_system_packages --downloadonly --downloaddir="$system_packages_dir" --setopt=installonly=False
+            repotrack -p "$system_packages_dir" $formated_system_packages
+        elif [ -f /etc/debian_version ]; then
+            # TODO
+            apt-get install --download-only -y $formated_system_packages -o Dir::Cache::archives="$system_packages_dir"
+        else
+            echo "Unsupported Linux distribution.. Please install the packages manually."
+        fi
     else
-        echo "Error: $HOME/.local/bin/env not found."
-        exit 1
+        echo "No system packages to install."
     fi
+}
 
-    echo "Installing Python $python_version using uv..."
-    uv python install "$python_version"
-    uv venv --python "$python_version" "$HOME/.venv$python_version"
+function install_python_packages() {
+    if [ -n "$python_packages" ]; then
+        echo "Installing uv and Python $python_version and packages: $python_packages"
 
-    echo "Installing Python packages..."
-    source "$HOME/.venv$python_version"/bin/activate
-    uv pip install $formated_python_packages -i https://pypi.tuna.tsinghua.edu.cn/simple
+        # Install uv with setup_env.sh
+        if ! command -v uv &> /dev/null; then
+            wget -O "$script_dir"/setup_env.sh https://raw.githubusercontent.com/taosdata/TDengine/main/packaging/setup_env.sh
+            chmod +x "$script_dir"/setup_env.sh
+            "$script_dir"/setup_env.sh install_uv
+        fi
 
-    echo "Copying the installed environment to $py_venv_dir..."
-    cp -r "$HOME/.venv$python_version" "$py_venv_dir"
-    cp -r "$HOME/.local" "$py_venv_dir"
-else
-    echo "No Python packages to install."
-fi
+        if [ -f "$HOME/.local/bin/env" ]; then
+            source "$HOME/.local/bin/env"
+        else
+            echo "Error: $HOME/.local/bin/env not found."
+            exit 1
+        fi
 
-tar zcf "$offline_env_dir.tar.gz" "$offline_env_dir"
-mv "$offline_env_dir.tar.gz" "$offline_env_path"
-echo "Offline env completed, please check $offline_env_path/$offline_env_dir.tar.gz"
+        echo "Installing Python $python_version using uv..."
+        uv python install "$python_version"
+        uv venv --python "$python_version" "$HOME/.venv$python_version"
+
+        echo "Installing Python packages..."
+        source "$HOME/.venv$python_version"/bin/activate
+        uv pip install $formated_python_packages -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+        echo "Copying the installed environment to $py_venv_dir..."
+        cp -r "$HOME/.venv$python_version" "$py_venv_dir"
+        cp -r "$HOME/.local" "$py_venv_dir"
+    else
+        echo "No Python packages to install."
+    fi
+}
+
+function summary() {
+    tar zcf "$offline_env_dir.tar.gz" "$offline_env_dir"
+    mv "$offline_env_dir.tar.gz" "$offline_env_path"
+    echo "Offline env completed, please check $offline_env_path/$offline_env_dir.tar.gz"
+}
+
+install_system_packages
+install_python_packages
+summary
