78bbd9b435031bb9165fbd44b344a59b71e89755
happyguoxy <happy_guoxy@163.com>
2025-06-26 15:35:03 +0800

    test:Add ci

--- .github/scripts/test-for-stream-dev.py
+++ .github/scripts/test-for-stream-dev.py
@@ -18,19 +18,6 @@ class TestRunner:
         self.timeout = self.utils.get_env_var('timeout_cmd')
         self.extra_param = self.utils.get_env_var('extra_param')
 
-    # def run_assert_test(self):
-    #     cmd = f"cd {self.wkc}/test/ci && ./run_check_assert_container.sh -d {self.wkdir}",
-    #     self.utils.run_command(cmd, silent=True)
-
-    # def run_void_function_test(self):
-    #     cmd = f"cd {self.wkc}/test/ci && ./run_check_void_container.sh -d {self.wkdir}",
-    #     self.utils.run_command(cmd, silent=True, check=False)
-
-    # def run_function_return_test(self):
-    #     print(f"PR number: {self.pr_number}, run number: {self.run_number}, extra param: {self.extra_param}")
-    #     cmd = f"cd {self.wkc}/test/ci && ./run_scan_container.sh -d {self.wkdir} -b {self.pr_number}_{self.run_number} -f {self.wkdir}/tmp/{self.pr_number}_{self.run_number}/docs_changed.txt {self.extra_param}",
-    #     self.utils.run_command(cmd, silent=True)
-
     def run_function_test(self):
         print(f"timeout: {self.timeout}")
         linux_cmds = [
@@ -38,27 +25,10 @@ class TestRunner:
             f"date",
             f"cd {self.wkc}/test/ci && {self.timeout} time ./run.sh -e -m /home/m.json -t ../tools/streamlist_for_ci.task -b PR-{self.utils.get_env_var('PR_NUMBER')}_{self.utils.get_env_var('GITHUB_RUN_NUMBER')} -l {self.wkdir}/log -o 1030 {self.utils.get_env_var('extra_param')}",
         ]
-        # mac_cmds = [
-        #     "date",
-        #     f"cd {self.wkc}/test && python3.9 -m venv .venv",
-        #     f"cd {self.wkc}/test && source .venv/bin/activate && pip install --upgrade pip",
-        #     f"cd {self.wkc}/test && source .venv/bin/activate && pip install -r requirements.txt",
-        #     f"cd {self.wkc}/test && source .venv/bin/activate && sudo TAOS_BIN_PATH={self.wk}/debug/build/bin WORK_DIR=`pwd`/yourtest DYLD_LIBRARY_PATH={self.wk}/debug/build/lib pytest --clean cases/01-DataTypes/test_datatype_bigint.py",
-        #     "date"
-        # ]
         if self.platform == 'linux':
             self.utils.run_commands(linux_cmds)
-        # elif self.platform == 'darwin':
-        #     self.utils.run_commands(mac_cmds)
 
     def run(self):
-        # if self.test_type == 'assert':
-        #     self.run_assert_test()
-        # elif self.test_type == 'void':
-        #     self.run_void_function_test()
-        # elif self.test_type == 'function_returns':
-        #     self.run_function_return_test()
-        # el
         if self.test_type == 'function':
             self.run_function_test()
             if self.platform == 'linux':
