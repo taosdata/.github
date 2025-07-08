058d1dd8bfa841365309ec2bf44d92cb36eee157
chenhaoran <haoran920c@163.com>
2025-07-08 20:01:10 +0800

    fix: update command execution for assert test to run in non-silent mode

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -20,7 +20,7 @@ class TestRunner:
 
     def run_assert_test(self):
         cmd = f"cd {self.wkc}/test/ci && ./run_check_assert_container.sh -d {self.wkdir}",
-        self.utils.run_command(cmd, silent=True)
+        self.utils.run_command(cmd, silent=False)
 
     def run_void_function_test(self):
         cmd = f"cd {self.wkc}/test/ci && ./run_check_void_container.sh -d {self.wkdir}",
