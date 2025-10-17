f53e73f0bec14cc5b0b83ff64f5375cdb45a0613
happyguoxy <happy_guoxy@163.com>
2025-10-17 17:47:39 +0800

    alter test.py

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -66,7 +66,7 @@ class TestRunner:
     def run_function_test(self):
         print(f"timeout: {self.timeout}")
         
-        self.merge_task_files()
+        # self.merge_task_files()
         # 获取环境变量并提供默认值
         pr_number = self.utils.get_env_var('PR_NUMBER') or 'unknown'
         run_number = self.utils.get_env_var('GITHUB_RUN_NUMBER') or '0'
@@ -81,7 +81,7 @@ class TestRunner:
         linux_cmds = [
             f"cd {self.wkc}/test/ci && export DEFAULT_RETRY_TIME=2",
             f"date",
-            f"cd {self.wkc}/test/ci && timeout 50000 time ./run.sh -e -m /home/m.json -t cases.task -b {branch_id} -l {self.wkdir}/log -o 1230 {self.utils.get_env_var('extra_param')}".strip(),
+            f"cd {self.wkc}/test/ci && timeout 36000 time ./run.sh -e -m /home/m.json -t cases.task -b {branch_id} -l {self.wkdir}/log -o 1230 {self.utils.get_env_var('extra_param')}".strip(),
         ]
         mac_cmds = [
             "date",
